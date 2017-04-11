4ace3e09dcadea92e97e6277a5d8bcedd11fca64
JacobWylie <jacobwylie@gmail.com>
2017-04-11 15:55:30 +0200

    adding pages

---
+++ geolocation.js
@@ -0,0 +1,50 @@
+$("#error").hide();
+$("#hud").show();
+
+if(navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(gotLocation);
+} else {
+	displayError("Your browser doesn't support geolocation.");
+}
+
+navigator.geolocation.getCurrentPosition(gotLocation, gotError);
+
+function gotLocation(currentPosition) {
+  $("#hud").hide();
+
+  var $wonders = $("span");
+  
+  $wonders.each(function(){
+    var wondersLatitude = $(this).data("lat");
+    var wondersLongitude = $(this).data("lon");
+    
+    var distanceInMiles = calculateDistance(currentPosition.coords.latitude, currentPosition.coords.longitude, wondersLatitude, wondersLongitude);
+    
+    $(this).text(distanceInMiles + " miles");
+  });
+}
+
+function gotError(error) {
+	var message;
+
+	switch(error.code) {
+		case error.PERMISSION_DENIED:
+		message = "You need to give permission to use your location.";
+		break;
+		case error.PERMISSION_UNAVAILABLE:
+		message = "There was an error getting your location from your device. Please refresh the page.";
+		break;
+		case error.TIMEOUT:
+		message = "It took too long getting your position.";
+		break;
+		default: 
+		message = "An unknown error has occured. Please refresh the page";
+		break;
+	}
+	displayError(message);
+}
+
+function displayError(message) {
+	$('#hud').hide();	
+  $("#error").text(message).slideDown("slow");
+}
